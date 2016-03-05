240e5d3983a5ba5287ea8d6e23f75015f442a0a4
Sorumi <Sorumi33@gmail.com>
2016-03-05 16:44:06 +0800

    Page init

---
+++ js/main.js
@@ -0,0 +1,93 @@
+/**
+ * Created by Sorumi on 16/3/2.
+ */
+window.onload = function() {
+    addAnimate();
+    updateNav();
+    navOnClick();
+}
+
+window.onscroll = function() {
+    updateNav();
+    updateLogo();
+    animatedReady();
+}
+
+function addAnimate() {
+    $(".time-line-info").addClass("animated-ready").attr("href","fadeIn");
+    $(".photo-wrapper").addClass("animated-ready").attr("href","zoomInUp");
+    $(".depart .icon-wrapper").addClass("animated-ready").attr("href","zoomIn");
+    $(".member-wrapper").addClass("animated-ready").attr("href","fadeIn");
+    $(".join-form").addClass("animated-ready").attr("href","fadeInUp");
+}
+
+function updateNav() {
+    var links = document.querySelectorAll(".menu a")
+
+    for(var i = 0; i < links.length; i++) {
+        var link = links[i];
+
+        // 获取被链接指向的部分
+        var section = document.querySelector(link.getAttribute("href"));
+        var sectionTop = section.offsetTop;
+        var sectionBottom = section.offsetTop + section.clientHeight;
+
+        // 检查 window.scrollY 是否在这部分中
+        if(window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+            $(link).addClass("active");
+        } else {
+            $(link).removeClass("active");
+        }
+    }
+
+}
+
+function updateLogo() {
+    var height = window.scrollY;
+    var logoSmall = $(".logo-small");
+    if (height>40) {
+        $(".nav").css("padding-top","20px").css("opacity",0.9).css("position","fixed");
+        logoSmall.removeClass("animated zoomOut");
+        logoSmall.addClass("animated zoomIn");
+        logoSmall.css("opacity","1");
+    }else{
+        $(".nav").css("padding-top","60px").css("opacity",1).css("position","absolute");
+        if (logoSmall.css("opacity") == "1"){
+            logoSmall.removeClass("animated zoomIn");
+            logoSmall.addClass("animated zoomOut");
+        }
+    }
+}
+
+function animatedReady() {
+    var ready = document.querySelectorAll(".animated-ready");
+
+    for(var i = 0; i < ready.length; i++) {
+        var ar = ready[i];
+        if((window.scrollY + window.innerHeight)> getTop(ar)) {
+            //console.log(ar.offsetTop);
+            var animateType = $(ar).attr("href");
+            $(ar).addClass("animated "+animateType);
+            $(ar).css("visibility","visible");
+        }
+
+    }
+}
+
+function getTop(e){
+    var offset=e.offsetTop;
+    if(e.offsetParent!=null) offset+=getTop(e.offsetParent);
+    return offset;
+}
+
+function navOnClick() {
+    $(".nav-icon").click(function(){
+        $(".menu").slideToggle("");
+    })
+}
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+    });
+});
